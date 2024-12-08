b1a37b35f5abb036bf2e18aa4921a72c2e661bd3
Mikhail <148149508+thien1ao@users.noreply.github.com>
2024-12-08 23:55:50 +0100

    Add files via upload

---
+++ code/src/Points.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Points {
+    public static void main(String[] args) {
+        
+        
+    }
+}
+
+class Point {
+    double x;
+    double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
